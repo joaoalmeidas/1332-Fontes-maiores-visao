9c99f63ce902625749465b151001e27ebe8e293c
joao <joao182cx@gmail.com>
2019-02-14 10:41:41 -0200

    Modificação do tamanho da fonte para 20 no panelControle.

--- src/VisaoFrame.java
+++ src/VisaoFrame.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -11,23 +12,32 @@ public class VisaoFrame extends JFrame {
 	
 	private static String[] tiposFonte = {"Serif", "SansSerif", "Monospaced"};
 	
-	private JPanel panelControle;
-	private VisaoPanel panelVisao;
+	private final JPanel panelControle;
+	private final VisaoPanel panelVisao;
 	
-	private JComboBox <String> comboFontes;
-	private JCheckBox checkNegrito;
-	private JButton aumentaFonte;
-	private JButton diminuiFonte;
+	private final JComboBox <String> comboFontes;
+	private final JCheckBox checkNegrito;
+	private final JButton aumentaFonte;
+	private final JButton diminuiFonte;
+	private final Font fonteControle;
 	
 	public VisaoFrame() {
 		
+		
+		
 		panelControle = new JPanel(new FlowLayout());
+		fonteControle = new Font("SansSerif", Font.PLAIN, 20);
 		
 		panelVisao = new VisaoPanel();
 		comboFontes = new JComboBox<String>(tiposFonte);
 		checkNegrito = new JCheckBox("Negrito");
 		aumentaFonte = new JButton("Aumenta Fonte");
 		diminuiFonte = new JButton("Diminui Fonte");
+		
+		comboFontes.setFont(fonteControle);
+		checkNegrito.setFont(fonteControle);
+		aumentaFonte.setFont(fonteControle);
+		diminuiFonte.setFont(fonteControle);
 		
 		panelControle.add(comboFontes);
 		panelControle.add(checkNegrito);
